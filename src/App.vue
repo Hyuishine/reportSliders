92dffa438e6cc906d48b9ad62867650a4d771afa
hyuishine <hyuishine@gmail.com>
2020-07-12 00:32:01 +0800

    将多图形输出换为cavas画布输出，原：svg Path输出，去除不起效的fullpage冗余代码

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,50 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2020-07-11 09:46:13
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-07-11 18:20:28
+ * @LastEditTime: 2020-07-12 00:27:04
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
  * @youWant: add you want
 --> 
 <template>
-  <trianglify></trianglify>
+  <full-page :options="options"
+             ref="fullpage">
+    <!-- <template slot-scope="scope"> -->
+      <div class="section active" v-if="isActive()">123</div>
+      <div class="section">456</div>
+      <div class="section">789</div>
+      <div class="section">1112</div>
+    <!-- </template> -->
+  </full-page>
 </template>
 
 <script>
-import trianglify from '@/components/trianglify'
 export default {
-  components: {
-    trianglify
-  },
   name: 'App',
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      options: {
+        scrollOverflow: true, // 超出不显示滚动条
+        scrollBar: false, // 滚动条
+        resize: true, // 自适应
+        loopBottom: true, // 到底时循环
+        menu: "#menu",
+        anchors: ['home', 'scheme', 'enterprise', 'about'], //定义锚链接 链接后缀
+        sectionsColor: ["rgba(255,255,255,0.2)",],// 每个页面的背景色
+      }
+    }
+  },
+  methods: {
+    // 是否处于活动状态？ 判断section是否有active类名
+    isActive () {
+      console.log('123')
+      // if (self.classList.contains('active')) {
+      //   return true
+      // } else {
+      //   return false
+      // }
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2020-07-11 09:46:13
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-07-11 18:33:05
+ * @LastEditTime: 2020-07-11 23:17:31
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -14,13 +14,16 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import trianglify from 'trianglify'
+import 'vue-fullpage.js/node_modules/fullpage.js/vendors/scrolloverflow'
+import VueFullPage from 'vue-fullpage.js'
 
 Vue.config.productionTip = false
-
+Vue.use(VueFullPage)
 new Vue({
   router,
   store,
   vuetify,
   trianglify,
+  VueFullPage,
   render: h => h(App)
 }).$mount('#app')
